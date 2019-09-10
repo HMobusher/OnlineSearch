57f643539d77f6c56b46ec205654f7a4b62c4925
HMobusher <hmobusher76@gmail.com>
2019-09-10 15:47:13 -0400

    testing

---
+++ src/main/java/com/mobusher/OnlineSearchApplication.java
@@ -0,0 +1,14 @@
+package com.mobusher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineSearchApplication.class, args);
+		//testing
+	}
+
+}
